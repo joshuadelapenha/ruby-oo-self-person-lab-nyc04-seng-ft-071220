f1e4efda489815b270bef0beec8aff93b9ae877a
Joshua D <joshuadelapenha@yahoo.com>
2020-08-17 03:27:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,7 +1,7 @@
 # your code goes here
 class Person
   attr_reader :name
-  attr_accessor :bank_account, :happiness
+  attr_accessor :bank_account, :happiness, :hygiene
   
   def initialize(name)
     @name = name
