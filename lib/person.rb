d41a97ef610b3246a260fd4e9428d400e75e4128
Joshua D <joshuadelapenha@yahoo.com>
2020-08-17 03:27:46 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -19,8 +19,8 @@ class Person
   
   def hygiene=(num)
     @hygiene=num
-    @happiness = 10 if @happiness > 10
-    @happiness = 0 if @happiness < 0 
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0 
   end
   
 end
